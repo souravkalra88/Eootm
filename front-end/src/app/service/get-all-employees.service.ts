1a39bf104f6c929cddf04d4b19338f938f430a5f
souravkalra88 <73485078+souravkalra88@users.noreply.github.com>
2022-12-21 17:31:43 +0530

    emp added

--- front-end/src/app/service/get-all-employees.service.ts
+++ front-end/src/app/service/get-all-employees.service.ts
@@ -8,15 +8,14 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class GetAllEmployeesService {
-  url = "https://qfqfrz1b62.execute-api.ap-south-1.amazonaws.com/employees/allemp";
+  url = "https://qfqfrz1b62.execute-api.ap-south-1.amazonaws.com/allemp";
   constructor(private http : HttpClient) {
    }
    allEmployeesData():Observable<any> {{
     console.log(environment.idToken);
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' +environment.idToken,
-      'access-control-allow-origin': '*',
+      'Authorization': 'Bearer ' +environment.idToken
     })
     return this.http.get(this.url, { headers: headers })
    }
