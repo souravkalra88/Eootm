eca3a369e4272508cc03a19f06eca89a47422aef
souravkalra88 <73485078+souravkalra88@users.noreply.github.com>
2022-12-22 14:40:47 +0530

    Pop backend integreated

---
+++ front-end/src/app/service/add-task-type.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddTaskTypeService {
+  url = "https://qfqfrz1b62.execute-api.ap-south-1.amazonaws.com/addTaskType";
+  constructor(private http : HttpClient) {
+   }
+
+   addTaskType(body: any):Observable<any> {{
+    // console.log(environment.idToken);
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' +environment.idToken
+    })
+   console.log(body)
+   
+    return  this.http.put(this.url,body,{ headers: headers })
+   }
+  }
+
+}
--- front-end/src/app/task-type/task-type.component.ts
+++ front-end/src/app/task-type/task-type.component.ts
@@ -3,6 +3,10 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 import { GetAllTaskTypesService } from '../service/get-all-task-types.service';
 import { NgForm } from '@angular/forms';
+import { AddTaskTypeService } from '../service/add-task-type.service';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -14,13 +18,14 @@ export class TaskTypeComponent implements OnInit {
   dtOptions: DataTables.Settings = {};
   dtTrigger: Subject<any> = new Subject<any>();
   tasksTypeData:any; 
-
+    
   displayStyleAdd = "none";
   displayStyleEdit = "none";
   displayStyleAddTask = "none";
   formModal: any;
+   
 
-  constructor(private allTaskTypeData: GetAllTaskTypesService) { 
+  constructor(private allTaskTypeData: GetAllTaskTypesService, private addTaskType: AddTaskTypeService,private router: Router) { 
    
   }
   
@@ -29,6 +34,7 @@ export class TaskTypeComponent implements OnInit {
     this.allTaskTypeData.allTaskTypesData().subscribe((data: any)=>{
       // console.log(data);
       this.tasksTypeData = data;
+      console.log(this.tasksTypeData);
       this.dtTrigger.next(void 0);
     });
 
@@ -47,10 +53,10 @@ export class TaskTypeComponent implements OnInit {
   console.log(form.value.newTaskTypeTitle);
   console.log(form.value.newTaskTypeDesc);
 
-  
-  
+
+  this.closeAddNewTask();
     }
-    this.closeAddNewTask(); 
+     
   }
 
    
@@ -71,13 +77,52 @@ openEditTask(){
 
 addNewTaskType(form: NgForm):void{
   if(form.valid){
+    let taskTypeName = form.value.newTaskTypeTitle
   console.log(form.value.newTaskTypeTitle);
   console.log(form.value.newTaskTypeDesc);
+    let taskTypeDesc = form.value.newTaskTypeDesc
+    let body = {
+      "tasktype": taskTypeName,
+      "description": taskTypeDesc,
+      "CurrentUser": environment.currentUser
+  } 
+  
+  // DataTables.Api.ajax.reload(); 
+  this.addTaskType.addTaskType(body).subscribe((response: any )=> {
+    console.log(response);
+     
+
+    this.allTaskTypeData.allTaskTypesData().subscribe((data: any)=>{
+     
+      this.tasksTypeData = data;
+      console.log(this.tasksTypeData);
+    
+     
+    });  
+
+    // table refresh
+
+
+    const currentRoute = this.router.url;
+
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([currentRoute]);  
+    }); 
+
+ 
+  })  
+
+ 
+
+  
   }
   
-  this.closeAddNewTaskType(); 
+  this.closeAddNewTaskType()
+   
+   
   }
 
+   
   editTaskType(form: NgForm):void{
      
   
@@ -91,6 +136,7 @@ openAddNewTaskType(){
 
 closeAddNewTaskType(){
   this.displayStyleAdd = "none";
+  
 }
 
 openEditTaskType(){
