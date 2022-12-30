ebb513744a7e9f6913cfa284cc629783a2466d3e
souravkalra88 <73485078+souravkalra88@users.noreply.github.com>
2022-12-30 18:00:22 +0530

    random pass generated

--- backend/employees/handler.py
+++ backend/employees/handler.py
@@ -4,10 +4,12 @@ from logging import Logger
 import boto3
 import os
 import boto3
+import random
+import string
 from botocore.exceptions import ClientError
 import requests
 from boto3 import resource
-from passlib import pwd
+
 from boto3.dynamodb.conditions import Key
 import uuid
 dynamodb = boto3.resource(
@@ -316,6 +318,7 @@ def create_user(event,response):
 
 def generate_random_password():
   
-  _pass = str(pwd.genword(length = 12 , charset = "ascii_72"))
+  characters = string.ascii_letters + string.digits + string.punctuation
+  _pass = ''.join(random.choice(characters) for i in range(16))
 
   return _pass
